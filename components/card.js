6bc545597499e564fdf23446075645ef1109cb23
Yerlin <yerlinreyes00@gmail.com>
2025-03-19 22:39:57 -0600

    made default module

---
+++ components/card.js
@@ -0,0 +1,23 @@
+import styles from "./card.module.css";
+import Image
+ from "next/image";
+import Tag from "./tag";
+export default function Card({ data }) {
+  const { image, title, state, opinion, date, recommendationStatus} =
+    data;
+  return (
+    <div className={styles.card}>
+
+      <div className={styles.image}>
+        <Image src={image} alt="module image" width={300} height={300} priority/>
+      </div>
+      <div className={styles.content}>
+        <h1>{title}</h1>
+        <Tag state={state}></Tag>
+        <p id={styles.date}>{date}</p>
+        <p>{opinion}</p>
+        <p>{recommendationStatus}</p>
+      </div>
+    </div>
+  );
+}
---
+++ components/tag.js
@@ -0,0 +1,18 @@
+import styles from './tag.module.css';
+
+export default function Tag({state}) {
+  const getClass = () => {
+    switch (state) { //that needs to be changed
+      case "Active":
+        return styles.active;
+      case "pendiente":
+        return styles.pendiente;
+      case "inactive":
+        return styles.inactivo;
+      default:
+        return "active"; // Default case if `estado` doesn't match any expected values
+    }
+  };
+  
+  return (<di> <p className={`${styles.tag} ${getClass()}`}> {state}</p> </di>);
+}
