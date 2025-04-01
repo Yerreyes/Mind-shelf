db62ff692d2e08f91044510c41faa3d1b0e3251d
Yerlin <yerlinreyes00@gmail.com>
2025-03-31 19:50:49 -0600

    improve the header and others styles

--- app/explore/page.js
+++ app/explore/page.js
@@ -1,19 +1,31 @@
 "use client";
 import CardCategorie from "@/components/categories/card-categorie";
 import { useEffect, useState } from "react";
+import styles from "./page.module.css";
 
+/** note: here it s posible to use suspense, as a server component, and that avoid to use isloading and use client */
 export default function Home() {
   const [itemsModules, setItemsModules] = useState({});
-  
+  const [isLoading, setIsLoading] = useState(true); // Estado para el indicador de carga
+
   useEffect(() => {
     const fetchData = async () => {
-      const res = await fetch("/api/modules", {
-        method: "GET",
-        headers: { "Content-Type": "application/json" },
-      });
+      try {
+        setIsLoading(true); // Mostrar el indicador de carga
+        const res = await fetch("/api/modules", {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        });
 
-      const data = await res.json();
-      setItemsModules(data);
+        const data = await res.json();
+        if (data) {
+          setItemsModules(data);
+        }
+      } catch (error) {
+        console.error("An error occurred while fetching data:", error);
+      } finally {
+        setIsLoading(false); // Ocultar el indicador de carga
+      }
     };
 
     fetchData();
@@ -21,15 +33,15 @@ export default function Home() {
 
   return (
     <>
-      <div>
-        <h1>Categorires</h1>
-        <h3> Explore Categories</h3>
-      </div>
-      <ul>
-        {Object.entries(itemsModules).map(([key, value]) => (
-          <CardCategorie key={key} categorie={key} data={value}></CardCategorie>
-        ))}
-      </ul>
+      {isLoading ? ( // Mostrar el indicador de carga mientras se cargan los datos
+        <p>Cargando datos...</p>
+      ) : (
+        <ul className= {styles.cardCategorie}>
+          {Object.entries(itemsModules).map(([key, value]) => (
+            <CardCategorie key={key} categorie={key} data={value}></CardCategorie>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
--- components/categories/card-categorie.js
+++ components/categories/card-categorie.js
@@ -1,22 +1,24 @@
-
 import CategorieItem from "./categorie-item";
 import styles from "./card-categorie.module.css";
 import Link from "next/link";
 
 //Contains the card of each category, and inside the items of the category
-export default function CardCategorie({ categorie, data}) {
+export default function CardCategorie({ categorie, data }) {
   return (
     <div className={styles.container}>
       <div className={styles.box}>
         <Link href={`module/create/?category=${categorie}`}>+</Link>
       </div>
       <h1>{categorie}</h1>
-      <ul>
+      <ul className={styles.itemsContainer}>
         {/** data is a object with diferentes catgories which contains a diferentes items in a array */}
-        {data.map((item) => ( 
+        {data.map((item) => (
           <CategorieItem key={item.id} item={item} />
         ))}
       </ul>
+      <div>
+        <Link href={`module`}>Explorar más</Link>
+      </div>
     </div>
   );
 }
